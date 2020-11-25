e8fc2e9a45a737b53e1b577980f70f6cc7ec3d82
IanCode09 <ianlombu@protonmail.com>
2020-11-25 22:52:23 +0700

    Testing Component Button Part 2

--- src/elements/Button/index.test.js
+++ src/elements/Button/index.test.js
@@ -1,9 +1,34 @@
 import React from "react"
 import { render } from "@testing-library/react"
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import Button from './index'
 
-test("Shoul not allowed click button if isDisabled is present", () => {
+test("Should not allowed click button if isDisabled is present", () => {
     const { container } = render(<Button isDisabled />)
 
     expect(container.querySelector("span.disabled")).toBeInTheDocument()
+})
+
+test("Should render loading/spinner", () => {
+    const { container, getByText } = render(<Button isLoading />)
+
+    expect(getByText(/loading/i)).toBeInTheDocument()
+    expect(container.querySelector("span")).toBeInTheDocument()
+})
+
+test("Should render <a> tag", () => {
+    const { container } = render(<Button type="link" isExternal />)
+
+    expect(container.querySelector("a")).toBeInTheDocument()
+})
+
+test("Should render <link> component", () => {
+    const { container } = render(
+        <Router>
+            <Button href="" type="link" />
+        </Router>
+    )
+
+    expect(container.querySelector("a")).toBeInTheDocument()
 })

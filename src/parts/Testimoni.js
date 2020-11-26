6f569def27e7ea729aa18864dacb91ac4d3889a8
IanCode09 <ianlombu@protonmail.com>
2020-11-26 17:12:27 +0700

    Component Testimony

--- src/parts/Testimoni.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Star from 'elements/Star'
-
-export default function Testimoni() {
-    return (
-        <Star value={5} width={35} height={35} spacing={4} />
-    )
-}

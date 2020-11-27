eec2165d1af960c66e33600c56b05a2cc2eb00eb
IanCode09 <ianlombu@protonmail.com>
2020-11-27 07:11:02 +0700

    Component Element Input NUmber Part 2

--- src/elements/Form/InputDate/index.js
+++ src/elements/Form/InputDate/index.js
@@ -26,9 +26,50 @@ export default function Number(props) {
         }
     }
 
+    const minus = () => {
+        value > min &&
+            onChange({
+                target: {
+                    name: name,
+                    value: +value - 1
+                }
+            })
+    }
+
+    const plus = () => {
+        value < max &&
+            onChange({
+                target: {
+                    name: name,
+                    value: +value + 1
+                }
+            })
+    }
+
     return (
-        <div>
-            
+        <div className={["input-number mb-3", props.outerClassName].join(" ")}>
+            <div className="input-group">
+                <div className="input-group-prepend">
+                    <span className="input-group-text minus" onClick={minus}>
+                        -
+                    </span>
+                </div>
+                <input
+                    min={min}
+                    max={max}
+                    name={name}
+                    pattern="[0-9]*"
+                    className="form-control"
+                    placeholder={placeholder ? placeholder : "0"}
+                    value={String(InputValue)}
+                    onChange={onChange}
+                />
+                <div className="input-group-append">
+                    <span className="input-group-text plus" onClick={plus}>
+                        +
+                    </span>
+                </div>
+            </div>
         </div>
     )
 }

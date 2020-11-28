10a2281d38b42ec806018b6a5c81c981b4903b77
IanCode09 <ianlombu@protonmail.com>
2020-11-28 12:00:55 +0700

    Part Page Detail Title Part 2

--- src/parts/PageDetailTitle.js
+++ src/parts/PageDetailTitle.js
@@ -4,13 +4,13 @@ import Breadcrumb from 'elements/BreadCrumb'
 
 export default function PageDetailTitle({ data, breadcrumb }) {
     return (
-        <section clascName="container spacing-sm">
+        <section className="container spacing-sm">
             <Fade bottom>
                 <div className="row align-items-center">
                     <div className="col">
                         <Breadcrumb data={breadcrumb} />
                     </div>
-                    <div clascName="col-auto text-center">
+                    <div className="col-auto text-center">
                         <h1 className="h2">{data.name}</h1>
                         <span className="text-gray-400">
                             {data.city}, {data.country}
